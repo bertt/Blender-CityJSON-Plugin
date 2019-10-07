b0b21da4d70a86c9300edea10cef16c9f3dcb2d5
Stelios Vitalis <steliosvitalis@gmail.com>
2019-10-07 14:53:36 +0200

    Add an export function (dummy)

--- CityJSON_Blender_parser.py
+++ CityJSON_Blender_parser.py
@@ -125,7 +125,7 @@ def read_some_data(context, filepath, use_some_setting):
     return {'FINISHED'}
 # ImportHelper is a helper class, defines filename and
 # invoke() function which calls the file selector.
-from bpy_extras.io_utils import ImportHelper
+from bpy_extras.io_utils import ImportHelper, ExportHelper
 from bpy.props import StringProperty, BoolProperty, EnumProperty
 from bpy.types import Operator
 
@@ -165,16 +165,59 @@ class ImportCityJSON(Operator, ImportHelper):
     def execute(self, context):
         return read_some_data(context, self.filepath, self.use_setting)
 
+def export_some_data(context, filepath, use_some_settings):
+    pass
+
+class ExportCityJSON(Operator, ExportHelper):
+    """CityJSON export tool"""
+    bl_idname = "export_test.some_data"  # important since its how bpy.ops.import_test.some_data is constructed
+    bl_label = "Export Some Data"
+
+    # ImportHelper mixin class uses this
+    filename_ext = ".json"
+
+    filter_glob: StringProperty(
+        default="*.json",
+        options={'HIDDEN'},
+        maxlen=255,  # Max internal buffer length, longer would be clamped.
+    )
+
+    # List of operator properties, the attributes will be assigned
+    # to the class instance from the operator settings before calling.
+    use_setting: BoolProperty(
+        name="Example Boolean",
+        description="Example Tooltip",
+        default=True,
+    )
+
+    #type: EnumProperty(
+    #    name="Example Enum",
+    #    description="Choose between two items",
+    #    items=(
+    #        ('OPT_A', "First Option", "Description one"),
+    #        ('OPT_B', "Second Option", "Description two"),
+    #    ),
+    #    default='OPT_A',
+    #)
+
+    def execute(self, context):
+        return write_some_data(context, self.filepath, self.use_setting)
+
 
 # Only needed if you want to add into a dynamic menu
 def menu_func_import(self, context):
     self.layout.operator(ImportCityJSON.bl_idname, text="CityJSON (.json)")
+
+def menu_func_export(self, context):
+    self.layout.operator(ExportCityJSON.bl_idname, text="CityJSON (.json)")
 
 
 def register():
     bpy.utils.register_class(ImportCityJSON)
     bpy.types.TOPBAR_MT_file_import.append(menu_func_import)
 
+    bpy.utils.register_class(ExportCityJSON)
+    bpy.types.TOPBAR_MT_file_export.append(menu_func_export)
 
 def unregister():
     bpy.utils.unregister_class(ImportCityJSON)
